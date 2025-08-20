5acdd99917beeb280f86d44a0e1c8344100f910f
brianrecoba <brian12fernandes@gmail.com>
2025-08-20 16:48:32 -0300

    feat: add component loading

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -6,8 +6,18 @@ import {
   Inter_700Bold,
 } from "@expo-google-fonts/inter";
 import { colors } from "@/theme/colors";
+import { Loading } from "@/components/Loading";
 
 export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <Loading />;
+  }
   return (
     <Stack
       screenOptions={{
--- src/app/transaction/[id].tsx
+++ src/app/transaction/[id].tsx
@@ -1,11 +1,16 @@
 import { Button, Text, View } from "react-native";
 import { router, useLocalSearchParams } from "expo-router";
+import { fontFamily } from "@/theme/fontFamily";
 
 export default function Transaction() {
   const params = useLocalSearchParams<{ id: string }>();
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text style={{ color: "#000" }}>ID: {params.id}</Text>
+      <Text
+        style={{ color: "#000", fontFamily: fontFamily.bold, fontSize: 60 }}
+      >
+        ID: {params.id}
+      </Text>
       <Button title="Voltar" onPress={() => router.back()} />
     </View>
   );
